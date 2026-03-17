a6ddfa35ce19d3714e55b8e35e0a4a65694db5d4
Linnea Gräf <nea@nea.moe>
2026-03-17 19:13:16 +0100

    init

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs { };
+  treefmt-nix = import sources.treefmt-nix;
+  treefmtEval = (
+    treefmt-nix.mkWrapper pkgs ({
+      projectRootFile = "shell.nix";
+      programs.nixfmt.enable = true;
+      programs.prettier.enable = true;
+    })
+  );
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    npins
+    lix
+    treefmtEval
+  ];
+}
